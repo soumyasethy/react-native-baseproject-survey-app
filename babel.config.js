4ba7e34c2652b218c1a20f98a7cd3e0f074990a0
Soumya Ranjan Sethy <sethy.soumyaranjan@gmail.com>
2020-02-03 10:57:41 +0530

    Added MobX, Navigation, Snackbar

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  sourceMaps: true,
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+  ],
 };
