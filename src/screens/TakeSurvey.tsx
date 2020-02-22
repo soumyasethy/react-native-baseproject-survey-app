8042620527ac6437afbc80690325332cef7f7ab4
Soumya Ranjan Sethy <sethy.soumyaranjan@gmail.com>
2020-02-22 22:14:27 +0530

    Added Exit Survey logic  & Submit Survey function

--- src/screens/TakeSurvey.tsx
+++ src/screens/TakeSurvey.tsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {TakeSurveyCard} from 'component-library';
 import {inject, observer} from 'mobx-react';
 import {storeType} from '../store/storeType';
+import {_goBack} from '../navigator/_goToPage';
 
 @inject(storeType.surveyStore)
 @observer
@@ -12,8 +13,15 @@ class TakeSurvey extends Component {
     const update = (answer: any) => {
       updateAnswer(answer);
     };
+    const onExit = () => {
+      _goBack();
+    };
     return (
-      <TakeSurveyCard data={test[activeSurveyIndex]} updateAnswer={update} />
+      <TakeSurveyCard
+        data={test[activeSurveyIndex]}
+        updateAnswer={update}
+        onExit={onExit}
+      />
     );
   }
 }
